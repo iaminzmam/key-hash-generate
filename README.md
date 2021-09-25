a8896a1b7574ef318b0ae7399d17b7da8f68969f
iaminzmam <iaminzmam@gmail.com>
2021-09-25 21:03:54 +0530

    hash function updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # key-hash-generate
+
+localhost: http://localhost:5001/devlearn-d2f6d/us-central1/hashGenerator?type=new&key=test
--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,40 @@
 const functions = require("firebase-functions");
-const bcrypt = require('bcrypt');
+const crypto = require ("crypto");
 
-const saltRounds = 10;
+const algorithm = "aes-256-cbc";
+
 
 exports.hashGenerator = functions.https.onRequest(async (request, response) => {
   // console.log('test: ', request.query);
-  const {key, type} = request.query;
+  const {key, type, start, end} = request.query;
+  const initVector = crypto.randomBytes(16);
+  const Securitykey = crypto.randomBytes(32);
   let resp = '';
   switch (type) {
     case 'get':
+      const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+      let decryptedData = decipher.update(key, "hex", "utf-8");
+      decryptedData += decipher.final("utf8");
+      console.log("Decrypted message: " + decryptedData);
       break;
     case 'new':
-      resp = await bcrypt.hash(key, saltRounds)
-      console.log('hash: ',key, resp);
+      const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+      let encryptedData = cipher.update(key, "utf-8", "hex");
+      encryptedData += cipher.final("hex");
+      resp = encryptedData;
+      console.log("Encrypted message: " + encryptedData);
       break
     default:
       break;
   }
 
-  response.send(resp !== '' ? resp : "Done!");
+  if(resp !== '') {
+    let s = start && start !== '' ? start : null;
+    let e = end && end !== '' ? end : null;
+    resp = start && end ? resp.slice(s, e) : resp;
+  } else {
+    resp = 'Done!'
+  }
+
+  response.send(resp);
 });
