13a255a83292d1ec805bb06bb505b302f836a4e7
iaminzmam <iaminzmam@gmail.com>
2021-09-25 21:27:50 +0530

    hmac added

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,21 @@
 const functions = require("firebase-functions");
 const crypto = require ("crypto");
 
-const algorithm = "aes-256-cbc";
+// const algorithm = "aes-256-cbc";
 
 
 exports.hashGenerator = functions.https.onRequest(async (request, response) => {
   // console.log('test: ', request.query);
   const {key, type, start, end} = request.query;
-  const initVector = crypto.randomBytes(16);
-  const Securitykey = crypto.randomBytes(32);
   let resp = '';
   switch (type) {
     case 'get':
-      const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
-      let decryptedData = decipher.update(key, "hex", "utf-8");
-      decryptedData += decipher.final("utf8");
-      console.log("Decrypted message: " + decryptedData);
       break;
     case 'new':
-      const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
-      let encryptedData = cipher.update(key, "utf-8", "hex");
-      encryptedData += cipher.final("hex");
-      resp = encryptedData;
-      console.log("Encrypted message: " + encryptedData);
+      const secret = 'InzamamulHaque';
+      const hmac = crypto.createHmac('sha256',secret);
+      hmac.update('inz:'+key);
+      resp = hmac.digest('hex');
       break
     default:
       break;
